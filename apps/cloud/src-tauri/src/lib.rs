31bcfdeb2ed56f488213db04cf466dc1f0d344dd
project-arktosmos <projectarktosmos@gmail.com>
2026-04-29 23:56:44 +0200

    mirror image cache command in cloud shell with mhaol-cloud dir

--- apps/cloud/src-tauri/src/lib.rs
+++ apps/cloud/src-tauri/src/lib.rs
@@ -1,7 +1,10 @@
+mod image_cache;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![image_cache::image_cache_resolve])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
